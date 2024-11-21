bb8b6ce10f4daa1b6c9fb5f9c4c3929c2165d796
Mariya Zaharieva <114507155+mariyaZaharieva951@users.noreply.github.com>
2024-11-21 14:31:33 +0000

    Added tailwind css, colors and background

---
+++ blog-card/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        white: '#FFFFFF',
+        grayLight1: '#F9FAFB',
+        grayLight2: '#D2D6DB',
+        infigo: '#4338CA',
+        black: '#171717',
+        grayDark: '#525252',
+        greenDark: '#15803D',
+        greenMedium: '#BBF7D0',
+        greenLight: '#F0FDF4' 
+      }
+    },
+  },
+  plugins: [],
+}
+
